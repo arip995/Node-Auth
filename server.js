453d66436887b452bf7bde4c86eb9d086255055f
arip995 <arindampanda2002@gmail.com>
2023-08-29 14:05:48 +0530

    to check deployment

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const fs = require('fs');
 const https = require('https');
+const http =require('http');
 const app = require('./src/app');
 
-https.createServer({
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem'),
-}, app).listen(8000, () => {
-    console.log("server listening at port 8000")
+// https.createServer({
+//     key: fs.readFileSync('key.pem'),
+//     cert: fs.readFileSync('cert.pem'),
+// }, app).listen(8000, () => {
+//     console.log("server listening at port 8000")
+// })
+
+http.createServer(app).listen(8000,()=>{
+    console.log('Server running at PORT 8000')
 })
